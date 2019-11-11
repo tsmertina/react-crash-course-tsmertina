414331b4e3c9179c06c28a681164c1b097491c7a
Tetyana Smertina <t.smertina@astoundcommerce.com>
2019-11-11 18:17:07 +0200

    task_2:FIX: Fixed logic, changed class-based component to function

--- src/components/RequestUI.jsx
+++ src/components/RequestUI.jsx
@@ -1,29 +1,54 @@
 import React from 'react';
 
-class RequestUI extends React.Component {
-    render(props) {
-        let {processing, error, handleRequestClick, handleCancelClick, info} = this.props;
-        return(
-             <>
-                <button onClick={handleRequestClick} disabled={processing}>Create Request</button>
-                <button onClick={handleCancelClick}>Cancel request</button>
-                {processing && 
-                    <p>Random name processing...</p>
-                }
-                <p>
-                    {
-                        error ? 
-                        <>
-                            {error}
-                            <br/>
-                            <button onClick={handleRequestClick}>Repeat request</button>
-                        </>
-                        : 
-                        info
-                    }
-                </p>
-            </>
-        )
-    }
+export default function RequestUI(props) {
+    let { processing, error, handleRequestClick, handleCancelClick, info } = props;
+    
+    return(
+        <>
+           <button onClick={handleRequestClick} disabled={processing}>Create Request</button>
+           <button onClick={handleCancelClick}>Cancel request</button>
+           {processing && 
+               <p>Random name processing...</p>
+           }
+           <p>
+               {
+                   error ? 
+                   <>
+                       {error}
+                       <br/>
+                       <button onClick={handleRequestClick}>Repeat request</button>
+                   </>
+                   : 
+                   info
+               }
+           </p>
+       </>
+   )
 }
-export default RequestUI;
+// class RequestUI extends React.Component {
+//     render(props) {
+//         let {processing, error, handleRequestClick, handleCancelClick, info} = this.props;
+//         return(
+//              <>
+//                 <button onClick={handleRequestClick} disabled={processing}>Create Request</button>
+//                 <button onClick={handleCancelClick}>Cancel request</button>
+//                 {processing && 
+//                     <p>Random name processing...</p>
+//                 }
+//                 <p>
+//                     {
+//                         error ? 
+//                         <>
+//                             {error}
+//                             <br/>
+//                             <button onClick={handleRequestClick}>Repeat request</button>
+//                         </>
+//                         : 
+//                         info
+//                     }
+//                 </p>
+//             </>
+//         )
+//     }
+// }
+// export default RequestUI;
